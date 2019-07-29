ae9bfa78067a23da97e3d778e1ef9b63e48dcba3
Vignesh N <vignesh.nanjundan@vvdntech.in>
2019-07-29 18:15:41 +0530

    to find the number is postive or not

---
+++ find_number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int num;
+	num = 3;
+	if(num != 0) {
+		if (num > 0)
+		printf("POSITIVE\n");
+		else
+		printf("NEGATIVE\n");
+	}
+	else
+	printf("ZERO\n");
+	return 0;
+}
